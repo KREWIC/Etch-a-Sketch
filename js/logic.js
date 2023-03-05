24e77f6d13de9433f5a4cd5b499152abff1863e5
Krewic <maxxpizzitola97@gmail.com>
2023-03-05 14:45:22 -0600

    Create some initial CSS to better highlight the grid system created by the JS

--- js/logic.js
+++ js/logic.js
@@ -6,17 +6,20 @@ const sketchScreen = document.querySelector('#sketchScreen');
 
 let column = [];
 let row = [];
+//^^ Initial arrays for rows and columns
 
 for (let j = 0; j<4; j++)
 
 {
     row[j] = document.createElement('div');
+    row[j].setAttribute('id','row');
     sketchScreen.append(row[j]);
     //^^ This for loop creates the initial container for each row
 
    for (let i = 0; i<4; i++ )
         {
          column [i]= document.createElement('div');
+         column [i].setAttribute('id','grid');
          row[j].append(column[i]);
         } 
         //^^This for loop creates each item in each row
