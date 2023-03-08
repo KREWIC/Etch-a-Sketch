07c6d739a98e4b8c08006855bdaf93184f23efd9
Krewic <maxxpizzitola97@gmail.com>
2023-03-08 11:54:12 -0600

    Add a slider to the ui. Add listner and function to change grid size based on slider position

--- js/logic.js
+++ js/logic.js
@@ -1,28 +1,68 @@
 const body = document.querySelector('body');
 const sketchScreen = document.querySelector('#sketchScreen');
+const rightContainer = document.querySelector('.rightButtons');
+const slider = document.querySelector('#myRange');
 
-//const row1 = document.createElement('div');
-//sketchScreen.append(row1);
+
+
+let slideNum = document.createElement('div');
+slideNum.setAttribute('class', 'slideNum');
+rightContainer.append(slideNum);
+//^^ Adds a display for the slider the user is changing
+
+console.log(document.getElementById('myRange').value)
+
+slideNum.textContent = slider.value;
+//^^ This will eventually set the slide number to be equal to the value of the slider
+
 
 let column = [];
 let row = [];
 //^^ Initial arrays for rows and columns
 
-for (let j = 0; j<4; j++)
+//___________________________________________________________________________
 
-{
-    row[j] = document.createElement('div');
-    row[j].setAttribute('id','row');
-    sketchScreen.append(row[j]);
-    //^^ This for loop creates the initial container for each row
-
-   for (let i = 0; i<4; i++ )
-        {
-         column [i]= document.createElement('div');
-         column [i].setAttribute('id','grid');
-         row[j].append(column[i]);
-        } 
-        //^^This for loop creates each item in each row
+function gridClear(){
+    for (let i = 0;i<row.length; i++)
+    {
+        row[i].remove('#row');
+    }
 }
-//^^ This code creates the divs needed for the initial grid.
+//^^ This will clear out the grid to prepare for the new grid value
 
+
+function gridChange(sliderNumber){
+
+    gridClear();
+
+    slideNum.textContent = slider.value + 'X' + slider.value;
+
+    for (let j = 0; j<sliderNumber; j++)
+
+    {
+        row[j] = document.createElement('div');
+        row[j].setAttribute('id','row');
+        sketchScreen.append(row[j]);
+        //^^ This for loop creates the initial container for each row
+
+        for (let i = 0; i<sliderNumber; i++ )
+            {
+             column [i]= document.createElement('div');
+             column [i].setAttribute('id','grid');
+             row[j].append(column[i]);
+            } 
+        //^^This for loop creates each item in each row
+    }
+
+}
+//^^ This creates the new grid
+
+gridChange(slider.value);
+//^^ Running this function once during initialization to spawn first grid
+
+
+slider.addEventListener('change', function(e){
+
+    gridChange(slider.value);
+    
+})
